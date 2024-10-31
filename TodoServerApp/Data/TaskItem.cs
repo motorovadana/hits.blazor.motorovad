e612dd0e4b0724e75e7efa1c76d32a06135f0e13
motorovadana <motorovadana@gmail.com>
2024-11-01 00:37:45 +0500

    1

---
+++ TodoServerApp/Data/TaskItem.cs
@@ -0,0 +1,15 @@
+﻿using System.Data;
+
+namespace TodoServerApp.Data
+{
+    public class TaskItem
+    {
+       public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public DateTime? CreatedData { get; set; }
+        public DateTime? FinishDate { get; set; }
+
+
+    }
+}
